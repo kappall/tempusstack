5553b8a8eb5d8f0694b62adbc3930b3fd4c2d795
Karam El labadie <145616025+kappall@users.noreply.github.com>
2025-06-22 19:18:56 +0200

    feature: detached option

--- bin/tempusstack.js
+++ bin/tempusstack.js
@@ -15,9 +15,37 @@ program
 program
   .command('up')
   .description('Start the services defined in tempusstack.yaml')
-  .action(() => {
-    const config = parseConfig();
-    up(config);
+  .option('-d --detached', 'Start all services in background')
+  .action(async (options) => {
+    try {
+        const config = parseConfig();
+        await up(config, options.detached);
+
+        if(!options.detached) {
+            console.log(chalk.blue('\nServices Started. Press Ctrl+c to stop and remove them.'));
+            process.stdin.resume();
+            process.on('SIGINT',async ()=>{
+                console.log(chalk.red('\n\nCtrl+c relevated. Starting cleaning...'));
+                // TODO: down
+                process.exit(0);
+            });
+        }
+    } catch (error) {
+        console.error(chalk.red(`Error during executions of command 'up': ${error.message || error}`));
+        process.exit(1);
+    }
   });
+
+program
+  .command('down')
+  .description('Stop and remove all services started by tempusstack')
+  .action(async () => {
+    try {
+
+    } catch (error) {
+        console.error(chalk.red(`Error during executions of command 'down': ${error.message || error}`));
+        process.exit(1);
+    }
+  })
 
 program.parse(process.argv);
--- core/orchestrator.js
+++ core/orchestrator.js
@@ -1,51 +1,108 @@
-const chalk = require('chalk');
-const Docker = require('dockerode')
+const chalk = require("chalk");
+const Docker = require("dockerode");
 
-const docker = new Docker()
+const docker = new Docker();
 
-async function up(config) {
-  console.log(chalk.green('Starting the services defined in tempusstack.yaml...\n'));
+async function up(config, detached = false) {
+  console.log(
+    chalk.green("Starting the services defined in tempusstack.yaml...\n")
+  );
+
+  const startedContainerIds = [];
 
   for (const [name, cfg] of Object.entries(config.services || {})) {
+    const containerName = `tempusstack_${name}`;
     console.log(chalk.blue(`- Startinng Service: ${name}`));
     console.log(`  image: ${cfg.image}`);
     console.log(`  port: ${cfg.port}`);
 
     try {
-        console.log(chalk.yellow(`  Checking and pulling the image ${cfg.image}...`));
-        await docker.pull(cfg.image);
-        console.log(chalk.green(`   Image ${cfg.image} ready.`));
-
-        const containerOptions = {
-            image: cfg.image,
-            name: `tempusstack_${name}`,
-            ExposedPorts: {},
-            HostConfig: {
-                PortBindings: {},
-            },
-            Env: Object.entries(cfg.env || {}).map(([key, value]) => `${key}=${value}`),
-        };
-
-        if(cfg.port) {
-            const protocol = 'tcp';
-            containerOptions.ExposedPorts[`${cfg.port}/${protocol}`] = {};
-            containerOptions.HostConfig.PortBindings[`${cfg.port}/${protocol}`] = [{HostPort: String(cfg.port)}];
+      let existingContainer;
+      try {
+        existingContainer = await docker.getContainer(containerName);
+        const inspectData = await existingContainer.inspect();
+        if (inspectData.State.Running) {
+          console.log(
+            chalk.yellow(
+              `  Container "${containerName}" already running. Skippinf start`
+            )
+          );
+          startedContainerIds.push(existingContainer.id);
+          continue;
+        } else {
+          console.log(
+            chalk.yellow(
+              `  Container "${containerName}" already exists but not running. Removing to recreate.`
+            )
+          );
+          await existingContainer.remove({ force: true });
         }
-        
-        console.log(chalk.yellow(`  Creating the container for ${name}...`));
-        const container = await docker.createContainer(containerOptions);
-        console.log(chalk.green(`   Container "${container.id.substring(0,12)}" created.`));
+      } catch (e) {
+        // container does not exsist. proceed to create
+      }
+      console.log(
+        chalk.yellow(`  Checking and pulling the image ${cfg.image}...`)
+      );
 
-        console.log(chalk.yellow(`  Starting the container for ${name}...`));
-        await container.start();
-        console.log(chalk.green(`   Service "${name}" started on port ${cfg.port || 'not mapped'}!`));
-        console.log('');
+      // TODO: progress bar
+      await new Promise((resolve, reject) => {
+        docker.pull(cfg.image, (err, stream) => {
+          if (err) return reject(err);
+          docker.modem.followProgress(stream, (err, res) =>
+            err ? reject(err) : resolve(res)
+          );
+        });
+      });
+      console.log(chalk.green(`   Image ${cfg.image} ready.`));
+
+      const containerOptions = {
+        image: cfg.image,
+        name: containerName,
+        ExposedPorts: {},
+        HostConfig: {
+          PortBindings: {},
+          RestartPolicy: { Name: "no" },
+        },
+        Env: Object.entries(cfg.env || {}).map(
+          ([key, value]) => `${key}=${value}`
+        ),
+      };
+
+      if (cfg.port) {
+        const protocol = "tcp";
+        containerOptions.ExposedPorts[`${cfg.port}/${protocol}`] = {};
+        containerOptions.HostConfig.PortBindings[`${cfg.port}/${protocol}`] = [
+          { HostPort: String(cfg.port) },
+        ];
+      }
+
+      console.log(chalk.yellow(`  Creating the container for ${name}...`));
+      const container = await docker.createContainer(containerOptions);
+      console.log(
+        chalk.green(`   Container "${container.id.substring(0, 12)}" created.`)
+      );
+      startedContainerIds.push(container.id);
+
+      console.log(chalk.yellow(`  Starting the container for ${name}...`));
+      await container.start();
+      console.log(
+        chalk.green(
+          `   Service "${name}" started on port ${cfg.port || "not mapped"}!`
+        )
+      );
+      console.log("");
     } catch (error) {
-        console.error(chalk.red(`   Error starting service "${name}":`));
-        console.error(chalk.red(`       ${error.message || error}`));
-        console.log('');
+      console.error(chalk.red(`   Error starting service "${name}":`));
+      console.error(chalk.red(`       ${error.message || error}`));
+      console.log("");
     }
   }
+  if (detached) {
+    console.log(chalk.green("All services have been started in backgound."));
+    console.log(chalk.grey("Use `tempusstack down` to stop and remove them.")); // TODO: tempusstack down
+  }
+
+  return startedContainerIds;
 }
 
 module.exports = { up };
